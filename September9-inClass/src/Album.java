0e8b5d5ae2f0601c6883518f0147a9bffe313fbe
Dan DeBlasio <dan@dandeblasio.com>
2020-09-09 11:46:55 -0600

    In Class Code

--- September9-inClass/src/Album.java
+++ September9-inClass/src/Album.java
@@ -1,4 +1,43 @@
 
 public class Album {
-
+  private String title;
+  private String artist;
+  private String[] track_name;
+  private int[] track_duration;
+  
+  public int album_duration() {
+    int total = 0;
+    for(int i=0; i<track_duration.length; i++) {
+      total += track_duration[i];
+    }
+    return total;
+  }
+  
+  int curLastTrack = 0;
+  public boolean setNextTrack(String inName, int inDuration) {
+    if(curLastTrack == track_name.length) return false;
+    track_name[curLastTrack] = inName;
+    track_duration[curLastTrack] = inDuration;
+    curLastTrack++;
+    return true;
+  }
+  
+  public Album(int numTrack, String inArtist, String inTitle) {
+    track_duration = new int[numTrack];
+    track_name = new String[numTrack];
+    artist = inArtist;
+    title = inTitle;
+  }
+  
+  public void print() {
+    System.out.println("Artists: " + artist);
+    System.out.println("Title: " + title);
+    
+    for(int i=0; i<track_duration.length; i++) {
+      // print "#. Name (time)"
+      System.out.println((i+1) + ". " + track_name[i] + " (" + track_duration[i] + ")");
+    }
+    System.out.println("Total duration: " + album_duration());
+  }
+  
 }
--- September9-inClass/src/driver.java
+++ September9-inClass/src/driver.java
@@ -2,7 +2,32 @@
 public class driver {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Album myAlbum = new Album(2, "Hey look an artists", "And it has a title");
+    // track 1, 73 seconds called "I made a track" 
+    System.out.println(myAlbum.setNextTrack("I made a track", 73));
+    System.out.println(myAlbum.setNextTrack("track 2", 85));
+    System.out.println(myAlbum.setNextTrack("The lost track", 700));
+    System.out.println("Total duration: " + myAlbum.album_duration());
+    myAlbum.print();
+    //System.out.println(myAlbum.title);
+    //myAlbum.title = "";
+    System.out.println("\n------\n");
+    
+    Album[] myCollection = new Album[4];//an array of 4 albums
+    myCollection[0] = myAlbum;
+    for(int i=1; i<myCollection.length; i++) {
+      // title is "title", arists is "artists" and 
+      //contain 1 track of 2 seconds called "empty feeling"
+      myCollection[i] = new Album(1, "artist","title");
+      myCollection[i].setNextTrack("empty feeling", 2);
+    }
+    //print second album in list
+    myCollection[1].print();
+    
+    System.out.println("\nMy entire collection:");
+    for(Album current: myCollection) {
+      current.print();
+    }
 
   }
 
